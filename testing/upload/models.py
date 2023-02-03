5033a15a08c1bac772edc3cdff7459fbe9b8cdbd
Aver <averkievei@gmail.com>
2023-02-03 14:15:00 +0300

    Create table

--- testing/upload/models.py
+++ testing/upload/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
-def test():
-    pass
+
+class tests(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+
+
+class questions(models.Model):
+    name = models.CharField(max_length=2000)
+    type = models.CharField(max_length=100)
+    order = models.IntegerField()
+
+
+class answers(models.Model):
+    answer = models.CharField(max_length=1000)
+    correct = models.BooleanField()
+    comments = models.TextField()
+    order = models.IntegerField()
