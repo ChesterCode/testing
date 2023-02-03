2e68f0ca224c6a553584245773c382c1c31e0a2e
Aver <averkievei@gmail.com>
2023-02-03 14:50:09 +0300

    Create table

--- testing/upload/models.py
+++ testing/upload/models.py
@@ -7,12 +7,14 @@ class tests(models.Model):
 
 
 class questions(models.Model):
+    test_name = models.CharField(max_length=150)
     name = models.CharField(max_length=2000)
     type = models.CharField(max_length=100)
     order = models.IntegerField()
 
 
 class answers(models.Model):
+    questions_name = models.CharField(max_length=2000)
     answer = models.CharField(max_length=1000)
     correct = models.BooleanField()
     comments = models.TextField()
