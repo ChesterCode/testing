bf69ad3ba87a6cad26d2ff6a3eece0325c8c0b70
AV <averkievei@gmail.com>
2023-02-25 16:12:36 +0300

    Проблема с импортом модели в views

--- testing/upload/views.py
+++ testing/upload/views.py
@@ -1,8 +1,12 @@
+import questions as questions
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import questions
 def index(request):
-    return HttpResponse("Главная страница")
+    questions = questions.objects.all()
+    res = "<h1>Список вопросов</h1>"
+    for item in questions:
+        res += f'<div>\n<p>{item.name}</p>\n</div\n<hr>'
+    return HttpResponse(res)
 
-def upload(request):
     return HttpResponse("Страница загрузки")
